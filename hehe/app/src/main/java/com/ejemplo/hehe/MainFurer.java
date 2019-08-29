8e9d22d254eadf85f77d2fec2314098f2461079a
minombre <tavo9887@hotmail.com>
2019-08-28 19:57:17 -0500

    kermit commit suicide

---
+++ hehe/app/src/main/java/com/ejemplo/hehe/MainFurer.java
@@ -0,0 +1,13 @@
+package com.ejemplo.hehe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFurer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_furer);
+    }
+}
